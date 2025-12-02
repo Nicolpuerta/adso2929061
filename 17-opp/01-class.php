6dba2dab166b7d4e8694a50b139aa1ec4bc25e2e
nicolpuerta <nicoldp.07@gmail.com>
2025-12-02 14:59:26 -0500

    opp

--- 17-opp/01-class.php
+++ 17-opp/01-class.php
@@ -1,52 +1,52 @@
 <?php
-$title = '01- Class';
-$description = 'Blueprint for creating objects with shared properties and behaviors';
+    $title= '01-Class';
+    $description ='Blueprint for creating objects with shared properties and behaviors.';
 
-include_once 'template/header.php';
-echo "<section>";
+    include_once 'template/header.php';
+    echo "<section>";
 
-class Vehicle
-{
-  # Attributes (Properties/characteristics)
-  public $brand;
-  public $refer;
-  public $model;
-  public $color;
+    class vehicle{
+        #Atributos (Caracteristicas o propiedades)
+        public $brand;
+        public $refer;
+        public $model;
+        public $color;
 
-  #methods (behaviors/actions)
-  public function setAttributes($b, $r, $m, $c)
-  {
-    $this->brand = $b;
-    $this->refer = $r;
-    $this->model = $m;
-    $this->color = $c;
-  }
+        public function setAttributes($b, $r, $m, $c){
+            $this->brand=$b;
+            $this->refer=$r;
+            $this->model=$m;
+            $this->color=$c;
+        }
 
-  public function getAttributes()
-  {
-    return "<ul>
-                  <li><strong>Brand:  </strong> $this->brand </li>
-                  <li><strong>Refer:  </strong> $this->refer </li>
-                  <li><strong>Model:  </strong> $this->model </li>
-                  <li><strong>Color:  </strong> $this->color </li>
-              </ul>";
-  }
-}
-# intance an object
-$vh1 = new Vehicle;
-$vh1->setAttributes('Volkswagen', 'Gold', 2020, 'Black');
-echo $vh1->getAttributes();
+        public function getAttributes(){
+            return "<ul>
+                        <li><strong>brand: </strong> $this->brand</li>
+                        <li><strong>Refer: </strong> $this->refer</li>
+                        <li><strong>Model: </strong> $this->model</li>
+                        <li><strong>Color: </strong> $this->color</li>
+                    </ul>";
+        }
 
-$vh2 = new Vehicle;
-$vh2->setAttributes('Toyota', 'Hilux', 2018, 'Green');
-$vh2->color = 'Blue';
-echo $vh2->getAttributes();
+    }
 
-$vh3 = new Vehicle;
-$vh3->brand = 'Mazda';
-$vh3->refer = 'CX30 ';
-$vh3->model = 2024;
-$vh3->color = 'Gray';
-echo $vh->getAttributes();
+    $vh1 = new vehicle;
+    $vh1->setAttributes('VolksWagen','Gold','2020','Black');
+    echo $vh1->getAttributes();
 
-include_once 'template/footer.php';
+
+    $vh2 = new vehicle;
+    $vh2->setAttributes('Toyota','Hilux','2018','Green');
+    $vh2->color='Blue';
+    echo $vh2->getAttributes();
+
+    $vh3 = new vehicle;
+    $vh3->brand='Mazda';
+    $vh3->refer='CX30';
+    $vh3->model='2024';
+    $vh3->color='Gray';
+    echo $vh3->getAttributes();
+
+
+
+    include_once 'template/footer.php';
